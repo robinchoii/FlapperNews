76ad911e67e80078135ae5b445da2d7516fd2b1b
robinchoii <robinchoii@gmail.com>
2016-03-21 17:45:38 -0700

    add home state

--- MEAN/app.js
+++ MEAN/app.js
@@ -1,16 +1,39 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router');
 
-app.controller('MainCtrl', [
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl'
+      });
+    $urlRouterProvider.otherwise('home');
+  }]);
+
+app.factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
+}]);
+
+
+app.controller(''MainCtrl'', [
   '$scope',
-  function($scope){
+  'posts',
+  function($scope, posts){
     $scope.test = "Hello world!";
-    $scope.posts = [
-      {title: 'Github', upvotes: 53, url: "https://www.github.com"},
-      {title: 'HackerNews', upvotes: 23, url: "https://news.ycombinator.com"},
-      {title: 'Twitter', upvotes: 25, url: "https://www.twitter.com"},
-      {title: 'Google', upvotes: 39, url: "https://www.google.com"},
-      {title: 'GeekWire', upvotes: 44, url: "https://www.geekwire.com"}
-    ];
+    $scope.posts = posts.posts
+    // $scope.posts = [
+    //   {title: 'Github', upvotes: 53, url: "https://www.github.com"},
+    //   {title: 'HackerNews', upvotes: 23, url: "https://news.ycombinator.com"},
+    //   {title: 'Twitter', upvotes: 25, url: "https://www.twitter.com"},
+    //   {title: 'Google', upvotes: 39, url: "https://www.google.com"},
+    //   {title: 'GeekWire', upvotes: 44, url: "https://www.geekwire.com"}
+    // ];
     $scope.addPost = function(){
       if(!$scope.title || $scope.title === '') {return;}
       $scope.posts.push({
